0a56bb7f200aa73243bf0d10594ff5961db0471c
승가리 <imsa1224@korea.ac.kr>
2020-01-02 15:05:34 +0900

    Revert "Merge branch 'dev' into taco"
    This reverts commit da8ea5d7dab1f6a858e313f0e8601031bb4a2943.

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 
 import topping from './topping';
 import feedback from './feedback';
-import user from './user';
 
 
 export default combineReducers({
   topping,
   feedback,
-  user,
 });
